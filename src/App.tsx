7a7952c9170b524324ddd9f84b8a23cb20bef68f
ARCTURUS <1638330246@qq.com>
2022-07-26 19:30:56 +0800

    feat: demo

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Calendar from 'src/components/calendar/calendar';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="calendar">
+        <Calendar></Calendar>
+      </div>
+    </div>
+  );
+}
+
+export default App;
