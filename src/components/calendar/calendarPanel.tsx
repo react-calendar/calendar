6b222e63680f3e564c9ed92ba87d14dfe911a0d0
ARCTURUS <1638330246@qq.com>
2022-07-27 13:02:16 +0800

    feat: 添加左右滑事件

--- src/components/calendar/calendarPanel.tsx
+++ src/components/calendar/calendarPanel.tsx
@@ -1,7 +1,6 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Swiper as SwiperClass } from 'swiper/types';
 import { useState } from 'react';
-import { initMonths } from 'src/utils/handler';
+import { initMonths, getDayWeekIdxInMonth, initMonth } from 'src/utils/handler';
 import CalendarDay from './calendarDay';
 
 import 'swiper/css';
@@ -13,33 +12,102 @@ interface PanelProps {
   date: DateFullType;
   startWeek: number;
   curTab: number;
-  onDateChange?: (value: DateFullType) => void;
-  onCurIndexChange?: (value: number) => void;
+  showLunar: boolean;
+  onDateChange: (value: DateFullType) => void;
+  onCurIndexChange: (value: number) => void;
 }
 
 function CalendarPanel(props: PanelProps) {
-  const { fold, date, startWeek, onDateChange, onCurIndexChange, curTab } =
-    props;
+  const {
+    fold,
+    date,
+    curTab,
+    startWeek,
+    showLunar,
+    onDateChange: odc,
+    onCurIndexChange: ocic,
+  } = props;
 
   const [months, setMonths] = useState<MonthType[]>(
     initMonths(date, curTab, startWeek)
   );
 
-  function refreshMonthPanel() {}
+  // 左滑事件
+  function prev(e: DateFullType) {
+    const ms = [...months];
+    const i = (curTab + 1) % 3; // 上上月索引
+    const ci = (curTab + 2) % 3; // 上月索引
+    ms[i] = initMonth(e.year, e.month - 1, e.day, startWeek);
+    ms[ci].trans =
+      getDayWeekIdxInMonth(
+        { year: e.year, month: e.month, day: e.day },
+        ms[ci].idays
+      ) * 44; // 更新上月偏移量
 
-  function refreshWeeksPanel() {}
-
-  if (fold) {
-    // 周视图
-    refreshWeeksPanel();
-  } else {
-    // 月视图
-    refreshMonthPanel();
+    setMonths(ms);
+    odc(e);
   }
 
-  function onSlide(e: SwiperClass) {
-    if (typeof onCurIndexChange === 'function') {
-      onCurIndexChange(e.realIndex);
+  // 右滑事件
+  function next(e: DateFullType) {
+    const ms = [...months];
+    const i = (curTab + 2) % 3; // 下下月索引
+    const ci = (curTab + 1) % 3; // 下月索引
+    ms[i] = initMonth(e.year, e.month + 1, e.day, startWeek);
+    ms[ci].trans =
+      getDayWeekIdxInMonth(
+        { year: e.year, month: e.month, day: e.day },
+        ms[ci].idays
+      ) * 44; // 更新下月偏移量
+
+    setMonths(ms);
+    odc(e);
+  }
+
+  // 切换时事件
+  function onSlide(e: any) {
+    if (e.swipeDirection /* 仅手(非点击)滑动时需要 */) {
+      const { day } = date;
+
+      const ms = [...months];
+      const m = ms[e.realIndex]; // 滑动结束后被激活的月份
+      const d = day <= m.count ? day : m.count;
+
+      const idx = m.idays.findIndex((el) => {
+        return el.state === 'cur' && el.day === d;
+      });
+
+      const d_ = m.idays[idx];
+
+      if (e.swipeDirection === 'prev') {
+        prev(d_);
+      } else if (e.swipeDirection === 'next') {
+        next(d_);
+      }
+    }
+
+    ocic(e.realIndex); // 通知父组件修改 curTab
+  }
+
+  // 点击改变本月日期时执行
+  function onDateChange(e: DateFullType) {
+    odc(e);
+
+    if (fold) {
+    } else {
+      const m = [...months];
+      // 当前点击的是本月, 则仅修改偏移量
+      for (let i = 0; i < m.length; i++) {
+        const { day } = e;
+        const d = day <= m[i].count ? day : m[i].count; // 防止超出上/下月范围
+        m[i].trans =
+          getDayWeekIdxInMonth(
+            { year: m[i].year, month: m[i].month, day: d },
+            m[i].idays
+          ) * 44;
+      }
+
+      setMonths(m);
     }
   }
 
@@ -59,12 +127,16 @@ function CalendarPanel(props: PanelProps) {
             style={{ transform: `translateY(-${fold ? item.trans : 0}px)` }}>
             {item.idays.map((i) => (
               <CalendarDay
+                fold={fold}
+                showLunar={showLunar}
+                onDateChange={onDateChange}
+                next={next}
+                prev={prev}
                 select={
                   i.year === date.year &&
                   i.month === date.month &&
                   i.day === date.day
                 }
-                onDateChange={onDateChange}
                 key={i.key}
                 date={i}></CalendarDay>
             ))}
