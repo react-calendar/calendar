7f791e61cac056be86ce66961b768203d7877cfc
ARCTURUS <1638330246@qq.com>
2022-07-27 13:01:44 +0800

    feat: 删掉默认值, 新增左右滑回调

--- src/components/calendar/calendarDay.tsx
+++ src/components/calendar/calendarDay.tsx
@@ -1,27 +1,48 @@
+import { useSwiper } from 'swiper/react';
+
 import 'src/styles/layout.scss';
 import 'src/styles/day.scss';
 
 interface DayProps {
   showLunar: boolean;
+  fold: boolean;
   date: DateFullType;
   select: boolean;
-  onDateChange?: (value: DateFullType) => void; // 点击回调
+  onDateChange: (value: DateFullType) => void; // 点击回调
+  prev: (value: DateFullType) => void; // 点击上月日期回调
+  next: (value: DateFullType) => void; // 点击下月日期回调
 }
 
 function CalendarDay(props: DayProps) {
-  const { date, showLunar, select } = props;
+  const { date, showLunar, select, fold, onDateChange, prev, next } = props;
 
+  const swiper = useSwiper();
+
+  // 点击日期事件
   function click(e: DateFullType) {
-    if (typeof props.onDateChange === 'function') {
-      props.onDateChange(e);
+    if (fold) {
+      // 当前处于周视图
+    } else {
+      // 当前处于月视图
+      if (e.state === 'prev') {
+        // 点击的是上个月
+        swiper.slidePrev();
+        prev(e);
+      } else if (e.state === 'next') {
+        // 点击的是下个月
+        swiper.slideNext();
+        next(e);
+      } else {
+        // 点击的是本月
+        onDateChange(e);
+      }
     }
   }
 
   return (
     <div
-      className={`column items-center justify-center day__container ${
-        date.state
-      } ${date.today ? 'today' : ''} ${select ? 'select' : ''}`}
+      className={`column items-center justify-center day__container 
+        ${date.state} ${date.today && 'today'} ${select && 'select'}`}
       onClick={() => click(date)}>
       {/* 日期 */}
       <div>{date.day}</div>
@@ -34,9 +55,5 @@ function CalendarDay(props: DayProps) {
     </div>
   );
 }
-
-CalendarDay.defaultProps = {
-  showLunar: true,
-};
 
 export default CalendarDay;
