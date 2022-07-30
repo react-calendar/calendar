c781762e4fa8cf0617ca02b5f03735064c098487
ARCTURUS <1638330246@qq.com>
2022-07-31 00:58:45 +0800

    feat: 添加事件回调

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import { markers } from 'src/test';
 
 import './styles/app.scss';
 
+function dateChange(e: DateFullType) {
+  console.log(e);
+}
+
 export default function App() {
   return (
     <div className="App">
       <div className="calendar">
-        <Calendar markers={markers}></Calendar>
+        <Calendar markers={markers} onDateChange={dateChange}></Calendar>
       </div>
     </div>
   );
