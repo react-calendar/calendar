a115b08b56137c9f815bd0c49b023d97e8a75ff4
ARCTURUS <1638330246@qq.com>
2023-07-12 22:56:51 +0800

    feat: add timezone

--- src/utils/date/dayjs.ts
+++ src/utils/date/dayjs.ts
@@ -5,6 +5,7 @@ import dayOfYear from 'dayjs/plugin/dayOfYear';
 import isBetween from 'dayjs/plugin/isBetween';
 import isToday from 'dayjs/plugin/isToday';
 import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
 
 dayjs.extend(isLeapYear);
 dayjs.extend(arraySupport);
@@ -12,5 +13,6 @@ dayjs.extend(dayOfYear);
 dayjs.extend(isBetween);
 dayjs.extend(isToday);
 dayjs.extend(utc);
+dayjs.extend(timezone);
 
 export default dayjs;
